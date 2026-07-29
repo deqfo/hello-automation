ecaf4e64ba8f44061d54d157220bafcf30d2dcbd
Kristián Eliaš <kristiandeqfo@gmail.com>
2026-07-29 10:10:14 +0200

    Added type hints and used mypy

--- main.py
+++ main.py
@@ -2,4 +2,10 @@
 import datetime as dt
 
 now = dt.datetime.now(dt.UTC)
-print(f"Hola! The current date and time is {now}")
+
+
+def greet(now: dt.datetime) -> str:
+    return f"Hola! The current date and time is {now}"
+
+
+print(greet(now))
