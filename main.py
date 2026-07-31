fc33f12316e28f1611f708177f7d680ac835829d
Kristián Eliaš <kristiandeqfo@gmail.com>
2026-07-31 10:28:07 +0200

    Added auto update

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ now = dt.datetime.now(dt.UTC)
 
 
 def greet(now: dt.datetime) -> str:
-    return f"Hola! The current date and time is {now}"
+    with open("greet.txt", "w") as f:
+        f.write(f"Hola! The current date and time is {now}")
 
 
-print(greet(now))
+greet(now)
